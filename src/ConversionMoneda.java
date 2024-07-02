fbdeb80352bf80791594fcacb1e358b7b0fe7f19
Ludwing Lamus <ludwingsantiago5lslo@gmail.com>
2024-07-02 18:00:08 -0500

    files updated

--- src/ConversionMoneda.java
+++ src/ConversionMoneda.java
@@ -1,2 +1,23 @@
+import java.util.Date;
+import java.util.Scanner;
+
 public class ConversionMoneda {
+    public static void convertirMoneda(String monedaSubject, String monedaTarget, ConversionRequest consulta, Scanner scan){
+        double valor;
+        double valorConvertido;
+        Date fecha = new Date();
+
+        Monedas monedas = consulta.ConsultarMoneda(monedaSubject, monedaTarget);
+        System.out.println("\n**************************************************************************************************");
+        System.out.println("* La tasa de conversion del día " +"'"+fecha+"'"+
+                " de "+monedaSubject+ " a "+monedaTarget+" es 1 "+monedaSubject+" = "+monedas.conversion_rate()+" "+monedaTarget+" *");
+        System.out.println("**************************************************************************************************\n");
+
+        System.out.print("Ingrese el valor a convertir " + monedaSubject+": ");
+        valor = Double.parseDouble(scan.nextLine());
+        valorConvertido = valor * monedas.conversion_rate();
+        System.out.println("La conversión de " + valor + " " + monedaSubject +
+                " a "+monedaTarget+" es de " + valorConvertido + " " + monedas.target_code()+"\n");
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        ConversionRequest consulta = new ConversionRequest();
+
+        int option = 0;
+        boolean notValid = true;
+
+        while (true) {
+            while (notValid) {
+                System.out.println("********************************************\n");
+                System.out.println("Bienvenido al conversor de monedas");
+                System.out.println("1) Dolar Estadounidense ==> Peso Argentino");
+                System.out.println("2) Peso Argentino ==> Dolar Estadounidense");
+                System.out.println("3) Dolar Estadounidense ==> Real Brasileño");
+                System.out.println("4) Real Brasileño ==> Dolar Estadounidense");
+                System.out.println("5) Dolar Estadounidense ==> Peso Colombiano");
+                System.out.println("6) Peso Colombiano ==> Dolar Estadounidense");
+                System.out.println("7) Salir");
+                System.out.println("-----------------------------------");
+                System.out.print("Opción: ");
+                try {
+                    option = scan.nextInt();
+                    notValid = false;
+                } catch (Exception e) {
+                    System.out.println("Solo se aceptan números enteros dentro del rango");
+                }
+                scan.nextLine();
+            }
+            notValid = true;
+            switch (option) {
+                case 1:
+                    ConversionMoneda.convertirMoneda("USD", "ARS", consulta, scan);
+                    break;
+                case 2:
+                    ConversionMoneda.convertirMoneda("ARS", "USD", consulta, scan);
+                    break;
+                case 3:
+                    ConversionMoneda.convertirMoneda("USD", "BRL", consulta, scan);
+                    break;
+                case 4:
+                    ConversionMoneda.convertirMoneda("BRL", "USD", consulta, scan);
+                    break;
+                case 5:
+                    ConversionMoneda.convertirMoneda("USD", "COP", consulta, scan);
+                    break;
+                case 6:
+                    ConversionMoneda.convertirMoneda("COP", "USD", consulta, scan);
+                    break;
+                case 7:
+                    System.out.println("Gracias por usar el servicio");
+                    System.out.println("Saliendo...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
     }
 }
